64cc2349f87c547f94cb0d4c556a3f2248f685b5
pduong96 <peter.duong@clio.com>
2024-11-30 22:26:25 -0700

    first pass, did not remember kotlin T.T

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,51 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val firstList = mutableListOf<Int>()
+        val secondList = mutableListOf<Int>()
+        var runningTotal = 0
+
+        input.forEach { line ->
+            val parts = line.split("   ")
+            firstList.add(parts[0].toInt())
+            secondList.add(parts[1].toInt())
+        }
+        firstList.sort()
+        secondList.sort()
+        firstList.forEachIndexed({ index, value ->
+            val difference = abs(value - secondList[index])
+            runningTotal += difference
+        })
+        return runningTotal
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val firstList = mutableListOf<Int>()
+        val secondList = mutableListOf<Int>()
+        var runningTotal = 0
+
+        input.forEach { line ->
+            val parts = line.split("   ")
+            firstList.add(parts[0].toInt())
+            secondList.add(parts[1].toInt())
+        }
+        val secondListToGroup = secondList.groupBy { it }
+        firstList.forEach { value ->
+            val numOfTimesInSecondList = secondListToGroup[value]?.size ?: 0
+            runningTotal += value * numOfTimesInSecondList
+        }
+        return runningTotal
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
+//    part1(input).println()
     part2(input).println()
 }
